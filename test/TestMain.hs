f22cba79588786690cd342ad4f40dfc3da593ce0
Andrey Chudnov <oss@chudnov.com>
2015-12-01 18:00:14 -0500

    Enabled expression parsing test cases

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -16,6 +16,7 @@ main = do unittests_ecmascript3 <- tests_unit
           defaultMain $ testGroup "all tests"
             [tests_ecmascript5_parser
             ,tests_ecmascript5_parser_with_autosemi
+            ,tests_ecmascript5_expression
             --,propertytest_ecmascript5_pretty
             ,unittests_ecmascript3
             ,unittests_ecmascript3_diff
