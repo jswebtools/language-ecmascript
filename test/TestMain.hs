4b5c0b435cb94f1ba8dbd28d18322e50d2759b30
Andrey Chudnov <oss@chudnov.com>
2015-12-01 18:18:29 -0500

    Test cleanup

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -3,23 +3,19 @@ module Main where
 
 import Test.Tasty 
 
-import Test.Unit
 import Test.Diff
 import Test.Pretty
 import Test.StatementTests
 import Test.ExpressionTests
           
-main = do unittests_ecmascript3 <- tests_unit
-          unittests_ecmascript3_diff <- tests_diff
+main = do tests_ecmascript5_diff <- tests_diff
           let propertytest_ecmascript5_pretty = tests_pretty
-          
           defaultMain $ testGroup "all tests"
             [tests_ecmascript5_parser
             ,tests_ecmascript5_parser_with_autosemi
             ,tests_ecmascript5_expression
-            --,propertytest_ecmascript5_pretty
-            ,unittests_ecmascript3
-            ,unittests_ecmascript3_diff
+            ,propertytest_ecmascript5_pretty
+            ,tests_ecmascript5_diff
             ]
             
           
